06d0807e4a81e9af7cc089e95a3fc3b3c0332bbd
Pedro Lazari <pedrom@leapthought.com>
2026-04-22 09:40:13 -0300

    Setup

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+[DllImport("NativeLib.dll")]
+static extern void HelloWorld();
+
+HelloWorld();
+Console.ReadLine();
+
+
